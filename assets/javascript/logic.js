91b56e9682c1f3d22b732d51cb6eb249f5b21d51
Robert <robertwinstonanderson@gmail.com>
2018-12-10 23:08:19 -0700

    add some logic for side nav

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,8 +4,8 @@
 //   });
 
   // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+  var collapsibleElem = document.querySelector('.collapsible');
+  var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
   // Or with jQuery
 
